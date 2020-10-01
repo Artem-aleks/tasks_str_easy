cca0b09092c128f42d8bba70057a4b3eb1de243b
SumarokovDaniil <44470355+SumarokovDaniil@users.noreply.github.com>
2020-10-01 11:47:29 +0300

    Update ft_cmp_str.py

--- ft_cmp_str.py
+++ ft_cmp_str.py
@@ -1,4 +1,15 @@
+def ft_len(str):
+    count = 0
+    for i in str:
+        count += 1
+    return count
+
+
 def ft_cmp_str(str1, str2, num):
     result_string = ''
-    result_string += str1[:num] + str2 + str1[num:]
+    for i in range(num):
+        result_string += str1[i]
+    result_string += str2
+    for i in range(num, ft_len(str1)):
+        result_string += str1[i]
     return result_string
