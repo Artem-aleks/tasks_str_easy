c90da21afdb9c1cdd924cc05022f7709d12cf0b4
SumarokovDaniil <44470355+SumarokovDaniil@users.noreply.github.com>
2020-10-01 11:10:40 +0300

    Update ft_first_end_three.py

--- ft_first_end_three.py
+++ ft_first_end_three.py
@@ -1,2 +1,2 @@
 def ft_first_end_three(string):
-    return string[0], string[1], string[2], string[-3], string[-2], string[-1] 
+    return string[0] + string[1] + string[2] + string[-3] + string[-2] + string[-1] 
