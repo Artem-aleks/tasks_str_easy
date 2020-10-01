d91644aef200b0919b5de2fb18127a7f289af0eb
SumarokovDaniil <44470355+SumarokovDaniil@users.noreply.github.com>
2020-10-01 11:56:32 +0300

    Update ft_slice_str.py

--- ft_slice_str.py
+++ ft_slice_str.py
@@ -8,6 +8,10 @@ def ft_len(str):
 
 def ft_slice_str(string, start, stop):
     result_string = ''
+    if stop > ft_len(string):
+        for i in range(1, ft_len(string)):
+            result_string += string[i]
+        return result_string
     for i in range(start, stop):
         result_string += string[i]
     return result_string
