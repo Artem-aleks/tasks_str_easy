4fc836a01d7a5f8f08891b89f2741f8b438f12fe
SumarokovDaniil <44470355+SumarokovDaniil@users.noreply.github.com>
2020-10-01 11:52:28 +0300

    Update ft_first_end_three.py

--- ft_first_end_three.py
+++ ft_first_end_three.py
@@ -1,2 +1,11 @@
+def ft_len(str):
+    count = 0
+    for i in str:
+        count += 1
+    return count
+
+
 def ft_first_end_three(string):
-    return string[0] + string[1] + string[2] + string[-3] + string[-2] + string[-1] 
+    if ft_len(string) > 5:
+        return string[0] + string[1] + string[2] + string[-3] + string[-2] + string[-1]
+    return string[0] * ft_len(string)
